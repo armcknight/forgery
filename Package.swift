985ea7c6bf42c50dfcd19b6c7c90ce90f7ae6f22
Andrew McKnight <git@mcknight.rocks>
2024-08-23 15:55:22 -0700

    remove stubbed test target

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -30,8 +29,5 @@ let package = Package(
                 .product(name: "Logging", package: "swift-log"),
             ]
         ),
-        .testTarget(
-            name: "forgeryTests",
-            dependencies: ["forgery"]),
     ]
 )
